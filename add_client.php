7b7e5ce3e956f9f4bcab52d6a990ab1aceaecee3
houssamlambara <lambarahoussam@gmail.com>
2024-12-11 16:55:59 +0100

    methode oriente Object

--- add_client.php
+++ add_client.php
@@ -3,12 +3,12 @@
 include('db.php');
 
 if($_SERVER['REQUEST_METHOD'] == "POST"){
-    $nom = $_POST['nom'];
-    $prenom = $_POST['prenom'];
-    $email = $_POST['email'];
-    $telephone = $_POST['telephone'];
-    $adresse = $_POST['adresse'];
-    $date_naissance = $_POST['date_naissance'];
+    $nom = $mysqli -> real_escape_string ($_POST['nom']);
+    $prenom = $mysqli -> real_escape_string ($_POST['prenom']);
+    $email = $mysqli -> real_escape_string ($_POST['email']);
+    $telephone = $mysqli -> real_escape_string ($_POST['telephone']);
+    $adresse = $mysqli -> real_escape_string ($_POST['adresse']);
+    $date_naissance = $mysqli -> real_escape_string ($_POST['date_naissance']);
 echo $nom . $prenom . $email . $telephone . $adresse . $date_naissance;
 
 $sql = "INSERT INTO `clients`(`NOM`,`PRENOM`,`EMAIL`,`TELEPHONE`,`ADRESSE`,`DATE_NAISSANCE`)VALUES('$nom','$prenom','$email','$telephone','$adresse','$date_naissance')";
--- add_reservations.php
+++ add_reservations.php
@@ -3,12 +3,12 @@
 include('db.php');
 
 if($_SERVER['REQUEST_METHOD'] == "POST"){
-    $titre = $_POST['titre'];
-    $destination = $_POST['destination'];
-    $prix = $_POST['prix'];
-    $date_debut = $_POST['date_debut'];
-    $date_fin = $_POST['date_fin'];
-    $place_disponible = $_POST['place_disponible'];
+    $titre = $mysqli -> real_escape_string ($_POST['titre']);
+    $destination = $mysqli -> real_escape_string ($_POST['destination']);
+    $prix = $mysqli -> real_escape_string ($_POST['prix']);
+    $date_debut =$mysqli -> real_escape_string ($_POST['date_debut']);
+    $date_fin = $mysqli -> real_escape_string ($_POST['date_fin']);
+    $place_disponible = $mysqli -> real_escape_string ($_POST['place_disponible']);
 echo $titre . $destination . $prix . $date_debut . $date_fin . $place_disponible;
 
 $sql = "INSERT INTO `reservations`(`DATE_RESERVATION`,`STATUS`)VALUES('$titre','$destination','$prix','$date_debut','$date_fin','$place_disponible')";
